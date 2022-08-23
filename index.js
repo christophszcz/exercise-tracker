e6a710f751251c731a4c6c09f31190cc04f8b765
Chris Szczechowicz <info@cjszczechowicz.com>
2022-08-22 23:59:43 -0400

    Started working on username endpoint

--- index.js
+++ index.js
@@ -9,6 +9,10 @@ app.get('/', (req, res) => {
   res.sendFile(__dirname + '/views/index.html');
 });
 
+app.post('/api/exercise/new-user', function(req, res) {
+  res.json({ username: 'test' });
+});
+
 const listener = app.listen(process.env.PORT || 3000, () => {
   console.log('Your app is here: ' + `http://localhost:${listener.address().port}`);
 })
