5f3d85cb5c9bc84831bcef760d32633b20d71785
Chris Szczechowicz <info@cjszczechowicz.com>
2022-08-25 23:58:36 -0400

    Create a GET request to /api/users to get a list of all users

--- index.js
+++ index.js
@@ -4,6 +4,7 @@ const cors = require('cors');
 require('dotenv').config();
 
 let idValue = 0;
+let users = [];
 
 app.use(cors());
 app.use(express.static('public'));
@@ -16,14 +17,19 @@ app.get('/', (req, res) => {
   res.sendFile(__dirname + '/views/index.html');
 });
 
-app.post('/api/users', function(req, res) {
+app.post('/api/users', (req, res) => {
   const urlResponse = { username: undefined, _id: undefined };
   const username = req.body.username;
   const id = idValue++;
 
   urlResponse.username = username;
   urlResponse._id = id;
+  users.push(urlResponse);
   res.json(urlResponse);
+});
+
+app.get('/api/users', (req, res) => {
+  res.json(users);
 });
 
 const listener = app.listen(process.env.PORT || 3000, () => {
